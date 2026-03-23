2e102d9698865b2f57630d1b402cb21b7db34aff
최종화 <cjh031012@gmail.com>
2026-03-23 16:34:12 +0900

    Create 실습과제5.cpp

---
+++ CH03/CH3-3/실습과제5/실습과제5.cpp
@@ -0,0 +1,31 @@
+// **********************************************
+// 제 목 : ch3-3 실습과제5
+// 날 짜 : 2026년 03월 23일
+// 작성자 : 2201053 최종화
+// **********************************************
+
+#include <iostream>
+using namespace std;
+class Sphere {
+	double radius;
+public:
+	Sphere();
+	Sphere(double a);
+	double getVolume();
+	~Sphere();
+};
+double Sphere::getVolume() {
+	return (4.0 / 3.0) * 3.14 * radius * radius * radius;
+}
+Sphere::Sphere(double a) :radius(a) { cout << "반지름" << radius << "인 구 생성" << endl; }
+Sphere::Sphere() :Sphere(1.0) {}
+Sphere::~Sphere() { cout << "반지름" << radius << "인 구 소멸" << endl; }
+
+
+Sphere sph1;
+Sphere sph2(3);
+int main() {
+	cout << "구의 부피는" << sph1.getVolume() << endl;
+	cout << "구의 부피는" << sph2.getVolume() << endl;
+	return 0;
+}
