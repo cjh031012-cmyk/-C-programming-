1a3d10fe01112dd4906aa34070b5a2f43f61be19
최종화 <cjh031012@gmail.com>
2026-03-25 10:40:44 +0900

    Add Triangle class implementation in 실습과제3.cpp

---
+++ CH03/CH3-4/실습과제3/실습과제3.cpp
@@ -0,0 +1,43 @@
+// **********************************************
+// 제 목 : ch3-4 실습과제3
+// 날 짜 : 2026년 03월 25일
+// 작성자 : 2201053 최종화
+// **********************************************
+
+#include <iostream>
+using namespace std;
+class Triangle {
+private:
+	double under;
+	double height;
+public:
+	Triangle();
+	void setWidth(double u);
+	void setHeight(double h);
+	double getWidth();
+	double getHeight();
+	double getArea();
+	~Triangle();
+};
+Triangle::Triangle() { under = 1; height = 1; cout << "폭" << under << "높이" << height << "삼각형생성" << endl; }
+void Triangle::setWidth(double u) { under = u; }
+void Triangle::setHeight(double h) { height = h; }
+double Triangle::getWidth() {
+	return under;
+}
+double Triangle::getHeight() {
+	return height;
+}
+double Triangle::getArea() {
+	return under * height * (0.5);
+}
+Triangle::~Triangle() { cout << "폭" << under << "높이" << height << "삼각형 소멸" << endl; }
+int main() {
+	Triangle tri;
+	tri.setWidth(3);
+	tri.setHeight(5);
+	cout << "삼각형의 폭은" << tri.getWidth() << endl;
+		cout << "삼각형의 높이는" << tri.getHeight() << endl;
+		cout << "삼각형의 높이는" << tri.getArea() << endl;
+		return 0;
+}
