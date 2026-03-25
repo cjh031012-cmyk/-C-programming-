7b6c25f086054ef95ebe8c5a3e402906d04b1741
최종화 <cjh031012@gmail.com>
2026-03-25 10:39:57 +0900

    Create 실습과제2.cpp

---
+++ CH03/CH3-4/실습과제2/실습과제2.cpp
@@ -0,0 +1,38 @@
+// **********************************************
+// 제 목 : ch3-4 실습과제2
+// 날 짜 : 2026년 03월 25일
+// 작성자 : 2201053 최종화
+// **********************************************
+#include <iostream>
+using namespace std;
+class Circle {
+private:
+	int radius;
+public:
+	Circle();
+	Circle(int i);
+	int getRadius();
+	void setRadius(int r);
+};
+Circle::Circle() { radius = 1; }
+Circle::Circle(int i) {
+	if (i <= 0) {
+		cout << "반지름은 양수여야합니다" << endl;
+		i = 1;
+	}
+	else {
+		i = radius;
+	}
+}
+int Circle::getRadius() {
+	return radius;
+}
+void Circle::setRadius(int r) {
+	radius = r;
+}
+int main() {
+	Circle waffle;
+	waffle.setRadius(5);
+	cout << "원의 반지름은" << waffle.getRadius() << endl;
+	return 0;
+}
